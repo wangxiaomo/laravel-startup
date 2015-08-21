9a059673b35dec4ffea2492250c5c74e319c570f
wangxiaomo <wxm4ever@gmail.com>
2015-08-21 15:17:03 +0800

    fix unittest

--- app/tests/ApiTest.php
+++ app/tests/ApiTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class ApiTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testBasicApi()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $resp = $this->call('GET', '/api');
+        $this->assertEquals(200, $resp->status());
     }
 }
